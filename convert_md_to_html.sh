33fdb7222569a428e5977240389f13c3c3dd28fa
gaowanlu <2209120827@qq.com>
2023-10-10 01:09:51 +0800

    feat: md convert2 html

---
+++ convert_md_to_html.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 函数用于将一个Markdown文件转换为HTML
+# sudo apt install pandoc
+convert_to_html() {
+  local input_file="$1"
+  local output_file="${input_file%.md}.html"
+  pandoc "$input_file" -o "$output_file"
+  echo "Converted $input_file to $output_file"
+}
+
+# 遍历当前文件夹下的所有.md文件并转换为HTML
+for md_file in $(find . -type f -name "*.md"); do
+  convert_to_html "$md_file"
+done
